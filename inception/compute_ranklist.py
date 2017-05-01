b9c47045840e145b957a58beb014cc968a936084
Tmbao <tmbao@apcs.vn>
2017-05-01 17:37:05 +0700

    Update compute_ranklist.py

--- inception/compute_ranklist.py
+++ inception/compute_ranklist.py
@@ -37,7 +37,12 @@ def _get_infer_op(logits):
     return tf.argmax(logits, 1)
 
 
-def _compute(test_dat):
+def _get_loss_op(logits, labels):
+    losses = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+    return tf.reduce_mean(losses)
+
+
+def _compute(valid_dat, test_dat):
 
     _log("test size = {}".format(test_dat.size()))
 
@@ -52,10 +57,13 @@ def _compute(test_dat):
 
         # Place holders
         inputs = tf.placeholder("float32", shape=(None, 2048))
+        labels = tf.placeholder("float32", shape=(None, 100))
 
         # Tensors
         forward_op = FCNet(dims=[2048, 2048, 100], graph=graph, beta=FLAGS.beta).forward(inputs)
         infer_op = _get_infer_op(forward_op)
+        reg_op = tf.add_n(tf.losses.get_regularization_losses())
+        loss_op = _get_loss_op(forward_op, labels) + reg_op
 
         saver = tf.train.Saver()
 
@@ -64,9 +72,36 @@ def _compute(test_dat):
             _log("restoring the model")
             saver.restore(sess, os.path.join(FLAGS.chkpnt_dir, "{0}-{0}".format(str(FLAGS.from_step))))
 
+        # Firstly, perform validation on valid_data
+        if FLAGS.verbose:
+            _log("-VALID- {} start".format(datetime.datetime.now()))
+            valid_dat.shuffle()
+            ground_truth = []
+            predictions = []
+            losses = []
+            for val_inputs, val_labels, _ in valid_dat.batches(FLAGS.batch_size):
+
+                # Create food
+                food = {labels: val_labels, inputs: np.squeeze(val_inputs), "keep_prob:0": 1}
+
+                infer_val, loss_val, = sess.run(
+                    [infer_op, loss_op], feed_dict=food)
+
+                ground_truth = np.concatenate(
+                    [ground_truth, np.argmax(val_labels, 1)])
+                predictions = np.concatenate([predictions, infer_val])
+                losses.append(loss_val)
+
+            val_acc = np.mean(np.equal(predictions, ground_truth))
+            val_loss = np.mean(np.array(losses))
+
+            _log("-VALID- {} done: loss={:.4}, acc={:.4}"
+                 .format(datetime.datetime.now(), val_loss, val_acc))
+
+
+        # Carry out prediction on test_data
         labels = []
         paths = []
-
         for val_inputs, _, val_paths in test_dat.batches(FLAGS.batch_size):
             # Create food
             food = {inputs: np.squeeze(val_inputs), "keep_prob:0": 1}
@@ -89,9 +124,11 @@ def _compute(test_dat):
 
 
 def main():
+    valid_dat = Data(FLAGS.data_dir, "valid", 1,
+                     no_categories=100, suffix=".inceptionv3.pool.npy")
     test_dat = Data(FLAGS.data_dir, "test", 1, is_test=True,
                      no_categories=100, suffix=".inceptionv3.pool.npy")
-    _compute(test_dat)
+    _compute(valid_dat, test_dat)
 
 
 if __name__ == "__main__":
